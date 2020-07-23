a2000ba92ac383f5bebedd46e93123c932744886
Ashwin Chalaka <ascha@Ashwins-MacBook-Pro.local>
2020-07-23 15:59:25 -0700

    Added first page and beginning of second page. Also added .gitignore file

--- server.py
+++ server.py
@@ -2,24 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# print(__name__)
 @app.route('/')
-def welcome1():
-	# return "Welcome to Ashwin's Portfolio!"
-	return render_template('homepage.html')
-
-# @app.route('/<name>')
-# def welcome2(name):
-# 	return "Welcome to my Portfolio " + name
-
-# @app.route('/aboutme')
-# def aboutme():
-# 	return "Welcome to About Me!"
-
-# @app.route('/resume')
-# def resume():
-# 	return "Welcome to Resume!"
-
+def welcome():
+	return render_template('index.html')
 
 if __name__=="__main__":
 	app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,3 @@
+function revealOptions(x) {
+    
+}
